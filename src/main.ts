ecae613f5be1eb5a561ea232d5ef255767e9f6c9
Leandro Gonaçalves <contato.leandrogoncalves@gmail.com>
2020-06-14 20:44:55 -0300

    docs: refactory main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,57 @@
-import 'module-alias/register'
-import 'dotenv/config'
+import "module-alias/register";
+import "dotenv/config";
 
-import { Logger, ValidationPipe, ValidationError } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
+import { Logger, ValidationPipe, ValidationError } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
-import { AppModule } from './app.module'
+import { AppModule } from "./app.module";
 
-import { FallbackExceptionFilter, HttpExceptionFilter, ValidationFilter } from '@/shared/filters'
-import { ValidationException } from '@/shared/exceptions'
+import {
+  FallbackExceptionFilter,
+  HttpExceptionFilter,
+  ValidationFilter,
+} from "@/shared/filters";
+import { ValidationException } from "@/shared/exceptions";
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     exposedHeaders: [
-      'Content-Length',
-      'Vojo-Authorization',
-      'Vojo-Client-Origin'
-    ]
-  })
+      "Content-Length",
+      "Vojo-Authorization",
+      "Vojo-Client-Origin",
+    ],
+  });
 
-  app.useGlobalFilters(new FallbackExceptionFilter(), new HttpExceptionFilter(), new ValidationFilter())
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    exceptionFactory: (errors: ValidationError[]) => {
-      let fields = {}
+  app.useGlobalFilters(
+    new FallbackExceptionFilter(),
+    new HttpExceptionFilter(),
+    new ValidationFilter()
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        let fields = {};
 
-      errors.map(err => {
-        fields = {
-          ...fields,
-          [err.property]: err.value ? 'Campo inválido.' : 'Campo obrigatório.',
-        }
-      })
+        errors.map((err) => {
+          fields = {
+            ...fields,
+            [err.property]: err.value
+              ? "Campo inválido."
+              : "Campo obrigatório.",
+          };
+        });
 
-      return new ValidationException(fields)
-    }
-  }))
+        return new ValidationException(fields);
+      },
+    })
+  );
 
-  await app.listen(port)
-  Logger.log(`Server started on port ${port}`, 'Bootstrap')
+  await app.listen(port);
+  Logger.log(`Server started on port ${port}`, "Bootstrap");
 }
 
-bootstrap()
+bootstrap();
